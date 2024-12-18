dc72bd140e8a046c9ebc45fd0ea2bc5a4599485b
Abimanianandan <abikbm06@gmail.com>
2024-12-18 11:56:15 +0530

    add icos in the products

--- src/components/bestseller/BestSeller.jsx
+++ src/components/bestseller/BestSeller.jsx
@@ -16,8 +16,13 @@ const BestSeller = () => {
   <div class="row hotdeals-row row-cols-1 row-cols-md-4 g-0 mt-3 d-flex justify-content-center">
     {/* card 1 */}
   <div class="col g-3 d-flex justify-content-center">
-    <div class="card d-flex justify-content-center">
+    <div class="card d-flex justify-content-center position-relative">
     <img src={bsimage1} class="card-img-top" alt="..."/>
+    <div className="icons-container position-absolute d-flex justify-content-center ms-4">
+         <i className="fa-regular fa-heart ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-down-left-and-up-right-to-center  ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-eye ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+      </div>
     <div class="card-body text-center">
       <h6 class='card-title'>Case with MagSafe</h6>
       <p class="card-text">$19.99 &nbsp; <a href=""><i class="fa-solid fa-cart-shopping" style={{color: "#000000"}}></i></a>
@@ -29,8 +34,13 @@ const BestSeller = () => {
   </div>
   {/* card 2 */}
   <div class="col g-3 d-flex justify-content-center">
-    <div class="card">
+    <div class="card position-relative">
     <img src={bsimage2} class="card-img-top" alt="..."/>
+    <div className="icons-container position-absolute d-flex justify-content-center ms-4">
+         <i className="fa-regular fa-heart ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-down-left-and-up-right-to-center  ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-eye ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+      </div>
     <div class="card-body text-center">
       <h6 class='card-title'>Case with MagSafe</h6>
       <p class="card-text">$19.99 &nbsp; <i class="fa-solid fa-cart-shopping" style={{color: "#000000"}}></i>
@@ -41,8 +51,13 @@ const BestSeller = () => {
   </div>
   {/* card 3  */}
   <div class="col g-3 d-flex justify-content-center">
-    <div class="card">
+    <div class="card position-relative">
     <img src={bsimage3} class="card-img-top" alt="..."/>
+    <div className="icons-container position-absolute d-flex justify-content-center ms-4">
+         <i className="fa-regular fa-heart ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-down-left-and-up-right-to-center  ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-eye ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+      </div>
     <div class="card-body text-center">
       <h6 class='card-title'>Case with MagSafe</h6>
       <p class="card-text">$19.99 &nbsp; <i class="fa-solid fa-cart-shopping" style={{color: "#000000"}}></i>
@@ -53,8 +68,13 @@ const BestSeller = () => {
   </div>
   {/* card 4 */}
   <div class="col g-3 d-flex justify-content-center">
-    <div class="card">
+    <div class="card position-relative">
     <img src={bsimage4} class="card-img-top" alt="..."/>
+    <div className="icons-container position-absolute d-flex justify-content-center ms-4">
+         <i className="fa-regular fa-heart ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-down-left-and-up-right-to-center  ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-eye ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+      </div>
     <div class="card-body text-center">
       <h6 class='card-title'>Case with MagSafe</h6>
       <p class="card-text">$19.99 &nbsp; <i class="fa-solid fa-cart-shopping" style={{color: "#000000"}}></i>
--- src/components/hotdeals/HotDeals.jsx
+++ src/components/hotdeals/HotDeals.jsx
@@ -13,59 +13,82 @@ const HotDeals = () => {
  
   <div className="hotdeals-container d-flex justify-content-center  mt-5">
 
-  <div class="row hotdeals-row row-cols-1 row-cols-md-4 g-4 mt-5 mt-3 d-flex justify-content-center">
+  <div className="row hotdeals-row row-cols-1 row-cols-md-4 g-4 mt-5 mt-3 d-flex justify-content-center">
     {/* card 1 */}
-  <div class="col g-3 d-flex justify-content-center">
-    <div class="card d-flex justify-content-center">
-    <img src={image1} class="card-img-top" alt="..."/>
-    <div class="card-body text-center">
-      <h6 class='card-title'>Case with MagSafe</h6>
-      <p class="card-text">$19.99
+  <div className="col g-3 d-flex justify-content-center">
+    <div className="card d-flex justify-content-center position-relative">
+    <img src={image1} className="card-img-top" alt="..."/>
+
+      <div className="icons-container position-absolute d-flex justify-content-center ms-4">
+         <i className="fa-solid fa-cart-shopping p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded" style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-regular fa-heart ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-down-left-and-up-right-to-center  ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-eye ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+      </div>
+    <div className="card-body text-center">
+      <h6 className='card-title'>Case with MagSafe</h6>
+      <p className="card-text">$19.99
       </p>
       
        </div>
     </div>
+   
+
   </div>
   {/* card 2 */}
-  <div class="col g-3 d-flex justify-content-center">
-    <div class="card">
-    <img src={image2} class="card-img-top" alt="..."/>
-    <div class="card-body text-center">
-      <h6 class='card-title'>Case with MagSafe</h6>
-      <p class="card-text">$19.99
+  <div className="col g-3 d-flex justify-content-center">
+    <div className="card position-relative">
+    <img src={image2} className="card-img-top" alt="..."/>
+    <div className="icons-container position-absolute d-flex justify-content-center ms-4">
+         <i className="fa-solid fa-cart-shopping p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded" style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-regular fa-heart ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-down-left-and-up-right-to-center  ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-eye ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+      </div>
+    <div className="card-body text-center">
+      <h6 className='card-title'>Case with MagSafe</h6>
+      <p className="card-text">$19.99
       </p>
       
        </div>
     </div>
   </div>
   {/* card 3  */}
-  <div class="col g-3 d-flex justify-content-center">
-    <div class="card">
-    <img src={image3} class="card-img-top" alt="..."/>
-    <div class="card-body text-center">
-      <h6 class='card-title'>Case with MagSafe</h6>
-      <p class="card-text">$19.99
+  <div className="col g-3 d-flex justify-content-center">
+    <div className="card posiion-relative">
+    <img src={image3} className="card-img-top" alt="..."/>
+    <div className="icons-container position-absolute d-flex justify-content-center ms-4">
+         <i className="fa-solid fa-cart-shopping p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded" style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-regular fa-heart ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-down-left-and-up-right-to-center  ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-eye ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+      </div>
+    <div className="card-body text-center">
+      <h6 className='card-title'>Case with MagSafe</h6>
+      <p className="card-text">$19.99
       </p>
       
        </div>
     </div>
   </div>
   {/* card 4 */}
-  <div class="col g-3 d-flex justify-content-center">
-    <div class="card">
-    <img src={image4} class="card-img-top" alt="..."/>
-    <div class="card-body text-center">
-      <h6 class='card-title'>Case with MagSafe</h6>
-      <p class="card-text">$19.99
+  <div className="col g-3 d-flex justify-content-center">
+    <div className="card position-relative">
+    <img src={image4} className="card-img-top" alt="..."/>
+    <div className="icons-container position-absolute d-flex justify-content-center ms-4">
+         <i className="fa-solid fa-cart-shopping p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded" style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-regular fa-heart ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-down-left-and-up-right-to-center  ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+         <i className="fa-solid fa-eye ms-2 p-2 shadow-lg p-3 mb-5 bg-body-tertiary rounded"  style={{backgroundColor:"whitesmoke", borderRadius:"5px"}}></i>
+      </div>
+    <div className="card-body text-center">
+      <h6 className='card-title'>Case with MagSafe</h6>
+      <p className="card-text">$19.99
       </p>
       
        </div>
     </div>
-  </div>
-  
-  
-
-  
+  </div>  
 </div>
   </div>
    </div>
